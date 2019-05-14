63e6f8c1f0d3c23f99e669d4fcda3e9c2963ca42
Andrew Young <young.andrew2@outlook.com>
2019-05-14 09:46:46 -0400

    Initial check-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SparkCoreAngularModule } from "@sparkdesignsystem/spark-core-angular";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './modules/homepage/homepage.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    SparkCoreAngularModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
